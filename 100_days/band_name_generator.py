d2b99e55cf088ed95903d735ccdba2d28d02d9a8
icebear <79372739+icebearhacks@users.noreply.github.com>
2022-07-13 16:19:35 -0400

    Add files via upload

---
+++ 100_days/band_name_generator.py
@@ -0,0 +1,9 @@
+# band name generator
+# The goal was to learn variables, user input, creating new lines, and concatenating strings
+
+print("Greetings! We're going to produce an awesome band name!")
+
+city = input("Which city did you grow up in?\n")
+pet = input("What's the name of a pet?\n")
+
+print('Your band name could be' + ' ' + city + ' ' + pet)
